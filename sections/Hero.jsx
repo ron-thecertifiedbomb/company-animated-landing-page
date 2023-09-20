b8960ab2a6963988b774658e6415d89ad832a61e
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2023-09-20 12:05:49 +0800

    fixed font

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -16,13 +16,13 @@ const Hero = () => (
     >
       <div className="flex justify-center items-center flex-col relative z-10">
         <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
-        Web Solutions
+        "Web Solutions
         </motion.h1>
         <motion.div
           variants={textVariant(1.2)}
           className="flex flex-row justify-center items-center"
         >
-          <h1 className={styles.heroHeading}>Worry-Free</h1>
+          <h1 className={styles.heroHeading}>Worry-Free"</h1>
         </motion.div>
       </div> 
     </motion.div>
